e25d917fac6f276f94cf4ee8d2ddbe9c814f8111
Gus Caplan <me@gus.host>
2017-10-26 08:45:12 -0500

    new function fuzzer

--- src/fuzzFunction.mjs
+++ src/fuzzFunction.mjs
@@ -1,6 +1,24 @@
 import { array } from './index';
 
 export default function fuzzFunction(fn, options = {}) {
-  const args = array(options);
-  fn(...args);
+  const errors = [];
+  for (let i = 0; i < (options.maxLength || 5); i++) {
+    const args = array(options);
+    try {
+      const ret = fn(...args);
+      if (options.returnTypes) {
+        for (const type of options.returnTypes) {
+          if (
+            (typeof type === 'string' && typeof ret !== type) ||
+            !(ret instanceof type)
+          ) {
+            errors.push(`arguments ${args} did not return one of the specified return types`);
+          }
+        }
+      }
+    } catch (err) {
+      if (!options.canError) errors.push(`arguments ${args} threw error ${err}`);
+    }
+  }
+  return errors;
 }
